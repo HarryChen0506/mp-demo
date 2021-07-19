86a35a9fb51bba8daac77f72a6fc7db74852f040
HarryChen0506 <chenwenhua0506@sina.com>
2021-07-19 13:56:15 +0800

    init projecrt

---
+++ src/libs/vc-util/index.js
@@ -0,0 +1,14 @@
+// utils
+export * as utils from './utils'
+export * from './utils'
+
+// voxios
+export * as voxios from './voxios'
+export {
+  Voxios,
+  defaultConfig as voxiosDefaultConfig
+} from './voxios'
+
+// hooks
+export * as hooks from './hooks'
+export * from './hooks'
